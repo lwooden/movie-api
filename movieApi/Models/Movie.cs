25b329095ba3cdeb15dc71c2692f5babaaf0ba8d
Lowell Wooden <lowell.wooden@gmail.com>
2021-02-24 22:35:50 -0500

    added new properties and migrate

--- movieApi/Models/Movie.cs
+++ movieApi/Models/Movie.cs
@@ -1,4 +1,7 @@
 ﻿using System;
+using System.ComponentModel.DataAnnotations.Schema;
+using Microsoft.AspNetCore.Http;
+
 namespace movieApi.Models
 {
     public class Movie
@@ -7,5 +10,14 @@ namespace movieApi.Models
         public int Id { get; set; }
         public string Name { get; set; }
         public string Language { get; set; }
+
+        public double Rating { get; set; }
+
+        // we do not want to store images in our database schema; only pointers to them (paths)
+        [NotMapped]
+        public IFormFile Image { get; set; }
+
+        public string ImageUrl { get; set; }
+
     }
 }
