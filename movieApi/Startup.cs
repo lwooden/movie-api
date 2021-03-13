6f8dbab4d34c34504303b98b3531cf610c4564bc
Lowell Wooden <lowell.wooden@gmail.com>
2021-03-12 22:56:28 -0500

    add support for JWT + disable xml serializer (broken)

--- movieApi/Startup.cs
+++ movieApi/Startup.cs
@@ -12,7 +12,10 @@ using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 using Microsoft.OpenApi.Models;
+using Microsoft.IdentityModel.Tokens;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
 using movieApi.Data;
+using System.Text;
 
 namespace movieApi
 {
@@ -30,7 +33,23 @@ namespace movieApi
         {
 
             services.AddControllers();
-            services.AddMvc().AddXmlSerializerFormatters();
+            // enable JWT support
+            services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+                .AddJwtBearer(options =>
+                {
+                    options.TokenValidationParameters = new TokenValidationParameters
+                    {
+                        ValidateIssuer = true,
+                        ValidateAudience = true,
+                        ValidateLifetime = true,
+                        ValidateIssuerSigningKey = true,
+                        ValidIssuer = Configuration["Tokens:Issuer"],
+                        ValidAudience = Configuration["Tokens:Issuer"],
+                        IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(Configuration["Tokens:Key"])),
+                        ClockSkew = TimeSpan.Zero,
+                    };
+                });
+            //services.AddMvc().AddXmlSerializerFormatters();
             services.AddDbContext<MovieDbContext>(options =>
                 options.UseSqlServer(
                     @"Data Source= (localdb)\MSSQLLocalDB;Initial Catalog=MovieDb;Integrated Security= True"));
@@ -54,7 +73,8 @@ namespace movieApi
             app.UseHttpsRedirection();
 
             app.UseRouting();
-
+            // place before authorization because authentication always comes before authorization
+            app.UseAuthentication();
             app.UseAuthorization();
             //dbContext.Database.EnsureCreated();
 
