d1837e925f42645681b298024f07cbd4229e8d02
Lowell Wooden <lowell.wooden@gmail.com>
2021-03-12 21:16:22 -0500

    new models

--- movieApi/Models/Movie.cs
+++ movieApi/Models/Movie.cs
@@ -1,6 +1,9 @@
 ﻿using System;
+using System.Collections;
+using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations.Schema;
 using Microsoft.AspNetCore.Http;
+using movieApi.Models;
 
 namespace movieApi.Models
 {
@@ -9,15 +12,33 @@ namespace movieApi.Models
 
         public int Id { get; set; }
         public string Name { get; set; }
+
+        public string Description { get; set; }
+
         public string Language { get; set; }
 
+        public string Duration { get; set; }
+
+        public DateTime PlayingDate { get; set; }
+
+        public DateTime PlayingTime { get; set; }
+
+        public double TicketPrice { get; set; }
+
         public double Rating { get; set; }
+
+        public string Genre { get; set; }
+
+        public string TrailerUrl { get; set; }
+
+        public string ImageUrl { get; set; }
 
         // we do not want to store images in our database schema; only pointers to them (paths)
         [NotMapped]
         public IFormFile Image { get; set; }
 
-        public string ImageUrl { get; set; }
+        // creates 1-to-Many Relationship between Movies<-->Reservations table (Foreign Key)
+        public ICollection<Reservation> Reservations { get; set; }
 
     }
 }
---
+++ movieApi/Models/User.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace movieApi.Models
+{
+    public class User
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public string Password { get; set; }
+        public string Role { get; set; }
+
+        // creates 1-to-Many Relationship between Users<-->Reservations table (Foreign Key)
+        public ICollection<Reservation> Reservations { get; set; }
+    }
+}
